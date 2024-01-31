c275f720cb2490bf8e9641a9cedef082281bed3b
Bensonmm <27972639+Bensonmm@users.noreply.github.com>
2024-01-31 03:30:30 -0800

    new updates

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,7 +3,7 @@
 Importing requests module
 """
 
-from requests import get
+import requests
 
 
 def number_of_subscribers(subreddit):
@@ -15,13 +15,17 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
+    user_agent = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'  # Using f-string for better readability
+    response = requests.get(url, headers=user_agent)
+
+    if response.status_code != 200:
+        return 0  # Return 0 if the request was not successful
+
     all_data = response.json()
 
     try:
-        return all_data.get('data').get('subscribers')
+        return all_data['data']['subscribers']
+    except KeyError:
+        return 0  # Return 0 if the key is not found in the response JSON
 
-    except:
-        return 0
